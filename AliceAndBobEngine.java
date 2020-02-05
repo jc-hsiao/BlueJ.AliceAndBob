4b72e41faeefa678da0a21221c728afe51b14e39
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-3.local>
2020-02-05 10:17:33 -0500

    finished

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -13,10 +13,30 @@ public class AliceAndBobEngine
         result = "";
     }
 
-    //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
+
+    
+    /*
+     * this function will check if the input is either Alice or Bob
+     * if it is, the function will return a greeting to them
+     * if not, the function will return a msg that says you are scary
+    */
     public String evaluateAliceOrBob(String input)
     {
-    
+        String a = "Alice";
+        String b = "Bob";        
+        String ansA = "Hello "+ a;
+        String ansB = "Hello "+ b;
+        String ansNO = "I DON'T KNOW YOU, YOU ARE SCARY";
+        String result = "";
+        
+        if(input.equals(a)){
+            result = ansA;
+        }else if(input.equals(b)){
+            result = ansB;
+        }else{
+            result = ansNO;
+        }        
+        
         return result;
     }
 }
